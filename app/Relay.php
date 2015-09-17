13105f1372d5dd2e2d890367b24ec0f9f9755082
Alex Wortham <alex@worthamworks.com>
2015-09-17 00:18:31 +0000

    Wrote OO Relay class.

---
+++ app/Relay.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use App\GPIO;
+
+class Relay
+{
+	const ON = 1;
+	const OFF = 0;
+	private const GPIO_OUTPUT_MODE = 1;
+	private $pin;
+	private $gpio;
+	private $state;
+
+	public function __construct($pin, $state = self::ON) {
+		$this->state = $state;
+		$this->gpio = new GPIO($pin, self::GPIO_OUTPUT_MODE);
+		$this->gpio->output($state);
+	}
+
+	public function on() {
+		$this->state = self::ON;
+
+		return $this->gpio->output(self::ON);
+	}
+
+	public function off() {
+		$this->state = self::OFF;
+
+		return $this->gpio->output(self::OFF);
+	}
+
+	public function get_state() {
+
+		return $this->state;
+	}
+
+	public function get_pin() {
+
+		return $this->pin;
+	}
+}
